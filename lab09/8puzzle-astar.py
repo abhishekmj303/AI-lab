12af273966dd91f1ae51bf5f979c2da58b143a45
Abhishek M J <abhishekmj303@gmail.com>
2023-03-25 16:58:58 +0530

    Update to working code - np-array only in required function - fix non-existent neighbour generation

--- lab09/8puzzle-astar.py
+++ lab09/8puzzle-astar.py
@@ -1,31 +1,17 @@
 import numpy as np
 
-initial = np.array([
-    [2, 8, 3],
-    [1, 6, 4],
-    [7, 0, 5]
-])
 
-final = np.array([
-    [1, 2, 3],
-    [8, 0, 4],
-    [7, 6, 5]
-])
-
-print(initial)
-print(final)
-
-
-def no_of_misplaced(current, final):
+def no_of_misplaced(current: tuple, final: tuple):
+    current, final = np.array(current), np.array(final)
     count = 0
     for i in range(1, len(final) ** 2):
         if np.where(current == i) != np.where(final == i):
             count += 1
-    print(count)
     return count
 
 
-def dist_to_final(current, final):
+def dist_to_final(current: tuple, final: tuple):
+    current, final = np.array(current), np.array(final)
     dist = 0
     for i in range(len(final)):
         for j in range(len(final)):
@@ -33,34 +19,39 @@ def dist_to_final(current, final):
                 cur_x, cur_y = np.where(current == final[i, j])
                 diff = [cur_x[0] - i, cur_y[0] - j]
                 dist += sum(np.abs(diff))
-    print(dist)
     return dist
 
 
-def generate_neighbors(current):
+def generate_neighbors(current: tuple):
+    current = np.array(current)
     empty = np.array(np.where(current == 0)).flatten()
     next_add = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     locations = [empty + np.array(a) for a in next_add]
     neighbors = []
-    print(len(current))
     for loc in locations:
-        if 0 <= loc[0] < len(current) and 0 <= loc[1] < len(current):
-            current_copy = np.copy(current)
-            print(loc)
-            current_copy[empty[0], empty[1]] = current_copy[loc[0], loc[1]]
-            current_copy[loc[0], loc[1]] = 0
-            neighbors.append(current_copy)
+        if np.any(loc < 0) or np.any(loc >= len(current)):
+            continue
+        current_copy = np.copy(current)
+        current_copy[empty[0], empty[1]] = current_copy[loc[0], loc[1]]
+        current_copy[loc[0], loc[1]] = 0
+        neighbors.append(tuple(map(tuple, current_copy)))
     return neighbors
 
 
-def print_path(parent, current):
-    if current.tobytes() in parent:
-        print_path(parent, parent[current.tobytes()])
-    current = np.frombuffer(current.tobytes(), dtype=np.int8).reshape(3, 3)
-    print(current)
+def print_puzzle(puzzle: tuple):
+    for row in puzzle:
+        row = ' '.join(map(str, row)).replace('0', ' ')
+        print(row)
+    print()
 
 
-def priority_enqueue(open_list, node):
+def print_path(parent: dict, current: tuple):
+    if current in parent:
+        print_path(parent, parent[current])
+    print_puzzle(current)
+
+
+def priority_enqueue(open_list: list, node: tuple):
     for i in range(len(open_list)):
         if node[1] < open_list[i][1]:
             open_list.insert(i, node)
@@ -68,35 +59,61 @@ def priority_enqueue(open_list, node):
     open_list.append(node)
 
 
-def astar(initial, final, heuristic):
-    n = len(final)
+def astar(initial: tuple, final: tuple, heuristic):
     open_list, closed_list = [], []
     parent = {}
     open_list.append((initial, 0 + heuristic(initial, final), 0))
 
-    while open_list:
-        current, f_n, g_n = open_list.pop()
+    max_iter = 0
+    while open_list and max_iter < 1000:
+        current, f_n, g_n = open_list.pop(0)
         closed_list.append(current)
-        if np.array_equal(current, final):
-            return parent
+
+        if current == final:
+            return parent, f_n
+
         neighbors = generate_neighbors(current)
+        g_n += 1
+
         for neighbor in neighbors:
-            in_open = 1 if np.isin(neighbor, [o[0] for o in open_list]).all() else 0
-            in_closed = 1 if np.isin(neighbor, [c[0] for c in closed_list]).all() else 0
+            in_open = 1 if neighbor in [o[0] for o in open_list] else 0
+            in_closed = 1 if neighbor in closed_list else 0
             if not in_closed:
-                g_n += 1
                 f_n = g_n + heuristic(neighbor, final)
                 if not in_open:
                     priority_enqueue(open_list, (neighbor, f_n, g_n))
-                    parent[neighbor.tobytes()] = current
+                    parent[neighbor] = current
                 else:
-                    index = open_list.index(neighbor)
+                    index = [o[0] for o in open_list].index(neighbor)
                     if f_n < open_list[index][1]:
                         open_list[index] = (neighbor, f_n, g_n)
-                        parent[neighbor.tobytes()] = current
+                        parent[neighbor] = current
+        max_iter += 1
+    if max_iter >= 1000:
+        print('Max iterations reached')
 
 
-no_of_misplaced(initial, final)
-dist_to_final(initial, final)
-parent_path = astar(initial, final, no_of_misplaced)
-print_path(parent_path, final)
+def main(hueristic, text):
+    initial = (
+        (2, 8, 3),
+        (1, 6, 4),
+        (7, 0, 5)
+    )
+
+    final = (
+        (1, 2, 3),
+        (8, 0, 4),
+        (7, 6, 5)
+    )
+
+    print(f'Heuristic used: {text}')
+    parent_path, moves = astar(initial, final, hueristic)
+    print('Steps: ')
+    print_path(parent_path, final)
+    print('No. of moves: ', moves)
+
+
+if __name__ == '__main__':
+    main(no_of_misplaced, 'Number of misplaced tiles')
+    print()
+    main(dist_to_final, 'Sum of the tile distances from goal')
